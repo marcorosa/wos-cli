71239ccfe018bf0bc0edbfb351873da2de8c8205
Marco Rosa <marcor165@hotmail.it>
2017-07-20 16:24:46 +0200

    pass arguments from main

--- src/search.py
+++ src/search.py
@@ -3,6 +3,7 @@ import texttable as tt
 import re
 
 from config import user_id, password
+from datetime import date
 from wos import WosClient
 
 
@@ -17,17 +18,24 @@ def _draw_table(data):
     print s
 
 
-def search():
+def search(author, years, results):
     client = WosClient(user_id, password)
     client.connect()
-    sp5 = client.search('AU=Surname Name',
-                        count=100,
-                        offset=1,
-                        timeSpan={'begin': '2012-01-01',
-                                  'end': '2018-01-01'})
+
+    # Build query
+    query = 'AU=%s' % author
+
+    # Build timespan
+    current_year = date.today().year
+
+    sq = client.search(query,
+                       count=results,
+                       offset=1,
+                       timeSpan={'begin': '%s-01-01' % (current_year - years),
+                                 'end': '%s-01-01' % (current_year + 1)})
 
     # Format xml
-    my_xml = re.sub(' xmlns="[^"]+"', '', sp5.records, count=1).encode('utf-8')
+    my_xml = re.sub(' xmlns="[^"]+"', '', sq.records, count=1).encode('utf-8')
     tree = ET.fromstring(my_xml)
 
     # Get results
